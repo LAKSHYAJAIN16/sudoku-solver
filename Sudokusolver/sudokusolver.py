7b9ff18c05422fe007d0982a1f58e710947d7c82
LAKSHYAJAIN16 <81555595+LAKSHYAJAIN16@users.noreply.github.com>
2021-10-16 20:07:35 +0530

    Delete sudokusolver.py

--- Sudokusolver/sudokusolver.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import os
-import numpy as np
-
-# a function to read the 
-def read_img(self):
-    print("Enter image name: ")
-    image_url = input()
-    self.img = cv2.imread(image_url, img = cv2.imread(image_url, cv2.IMREAD_GRAYSCALE)
